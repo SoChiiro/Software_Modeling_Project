b53a9af049b0506504b24c8f0787c62a88e1edb7
Senhua <senhua.liu@efrei.net>
2023-06-21 12:35:01 +0200

    dropdown Select course

--- src/components/Enrollment.jsx
+++ src/components/Enrollment.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, Fragment  } from 'react';
+import App from '../App.css';
+ 
 const Enrollment = () => {
+
   const [courses, setCourses] = useState([]);
   const [selectedCourse, setSelectedCourse] = useState('');
   const [enrollmentStatus, setEnrollmentStatus] = useState('');
@@ -41,30 +43,21 @@ const Enrollment = () => {
       setEnrollmentStatus('Enrollment failed. Please try again.');
     }
   };
-
-  return (
-    <div>
-      <h2>Enrollment</h2>
-      <select
-        value={selectedCourse}
-        onChange={(e) => setSelectedCourse(e.target.value)}
-      >
-        <option value="">Select a course</option>
-        {/* Pour tester */}
-        <option value="">Java</option>
-        <option value="">PHP</option>
-        {courses.map((course) => (
-          <option key={course.id} value={course.id}>
-            {course.title}
-          </option>
-        ))}
-      </select>
-      <button onClick={handleEnroll} disabled={!selectedCourse}>
-        Enroll
-      </button>
-      {enrollmentStatus && <p>{enrollmentStatus}</p>}
-    </div>
-  );
+return (
+  <div>
+    <div className="title">
+      <p className='text-black'>Select a course</p>
+      </div>
+    <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <option selected>Choose a course</option>
+      <option value="US">{courses}</option>
+      <option value="CA">Canada</option>
+      <option value="FR">France</option>
+      <option value="DE">Germany</option>
+    </select>
+</div>
+);
+   
 };
 
 export default Enrollment;
