829527c62a6064c12dca632b962d66912afa58e8
Senhua <senhua.liu@efrei.net>
2023-06-21 10:52:32 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Course from './components/Course';
 import Enrollment from './components/Enrollment';
 import Scheduling from './components/Scheduling';
@@ -9,12 +10,15 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <div>
-      <Course/>
-      <Enrollment />
-      <Scheduling />
-      <TrainingPlan />
-      <Evaluation />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Course />} />
+          <Route path="/enrollment" element={<Enrollment />} />
+          <Route path="/scheduling" element={<Scheduling />} />
+          <Route path="/trainingplan" element={<TrainingPlan />} />
+          <Route path="/evaluation" element={<Evaluation />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
